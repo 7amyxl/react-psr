6fe81507f097bc61eaeeecaaf2b0c51d09d8d20e
Disala Sennath <sennathdesilva@gmail.com>
2026-02-15 22:19:53 +0530

    commited myagent

--- my-react-practice/src/Button.jsx
+++ my-react-practice/src/Button.jsx
@@ -1,15 +1,31 @@
-import { useEffect, useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 
 function Button({countToDisplay}){
-  const [count,setCount] = useState(0);
+  // const [count,setCount] = useState(0);
+  // const [name,setName] = useState('');
+
+  const reduce = (state,action) =>{
+    if(action.type == 'increment'){
+      return {count: state.count + 1}
+    }
+    else{
+      return {count: state.count - 1}
+    }
+  }
+
+  const [state,dispatch] =  useReducer(reduce,{count: 2})
+  
 
   useEffect(() =>{
-    countToDisplay(count);
-  },[count])
+    countToDisplay(state.count);
+  },[state.count])
     return(
         <>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is
+        <button onClick={() => dispatch({type: 'increment'})}>
+          Increment
+        </button>
+        <button onClick={() => dispatch({type: 'decrement'})}>
+          Decrement
         </button>
         </>
     )
